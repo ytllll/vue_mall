b531ded835a501f0c43a671fed04c95286707864
ytlll <1340876809@qq.com>
2021-05-29 13:46:00 +0800

    '补充'

--- src/components/content/goods/GoodsListItem.vue
+++ src/components/content/goods/GoodsListItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="goods-item">
-    <img :src="showImage" alt="" @load="imgLoad" @click="itemClick">
+    <img v-lazy="showImage" alt="" @load="imgLoad" @click="itemClick">
     <div class="goods-info">
       <p>{{goodsItem.title}}</p>
       <span class="price">{{goodsItem.price}}</span>
